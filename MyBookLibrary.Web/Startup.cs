4a0a74ab1a55efbcbf80579c66bac291a1ed1b71
Simon Ng <simon.ng@fairfaxmedia.com.au>
2015-12-01 23:50:58 +1100

    Adding in Web Project

---
+++ MyBookLibrary.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBookLibrary.Web.Startup))]
+namespace MyBookLibrary.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
