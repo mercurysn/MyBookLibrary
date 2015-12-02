ab285c16c075a6de1d242bb833b4f13dcec2b25f
Simon Ng <simon.ng@fairfaxmedia.com.au>
2015-12-02 23:27:49 +1100

    Integrating Triangle Template

--- MyBookLibrary.Web/App_Start/BundleConfig.cs
+++ MyBookLibrary.Web/App_Start/BundleConfig.cs
@@ -8,24 +8,22 @@ namespace MyBookLibrary.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/layout-css").Include(
+                "~/Content/bootstrap1.min.css",
+                "~/Content/font-awesome.min.css",
+                "~/Content/animate.min.css",
+                "~/Content/lightbox.css",
+                "~/Content/main.css",
+                "~/Content/responsive.css"
+                ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/layout-scripts").Include(
+                "~/Scripts/jquery.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/lightbox.min.js",
+                "~/Scripts/wow.min.js",
+                "~/Scripts/main.js"
+                ));
         }
     }
 }
