8ebcaec26afcff8693e9aad665c360f0569a8384
Simon Ng <mercury_sn@hotmail.com>
2016-02-27 00:12:57 +1100

    Add in copy job

---
+++ MyBookLibrary.Data/Database/copy-job.bat
@@ -0,0 +1,2 @@
+xcopy /Y "C:\Source\MyBookLibrary\MyBookLibrary.Data\Database\BookImageFree.json" "C:\Users\mercu\Dropbox\MyBookLibrary\"
+xcopy /Y "C:\Source\MyBookLibrary\MyBookLibrary.Data\Database\Book.json" "C:\Users\mercu\Dropbox\MyBookLibrary\"
