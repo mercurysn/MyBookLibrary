855fa1f7af00220db584239af58f64212e5f9759
Simon Ng <simon.ng@fairfaxmedia.com.au>
2016-11-03 13:17:58 +1100

    Adding in S3UploadClient

--- MyBookLibrary.GoogleSheetDownloader/Program.cs
+++ MyBookLibrary.GoogleSheetDownloader/Program.cs
@@ -12,6 +12,20 @@ namespace MyBookLibrary.GoogleSheetDownloader
         {
             Mapper.Initialize(cfg => cfg.AddProfile(new MapToModelProfile()));
 
+            DownloadGoogleBookFile();
+
+            UploadFileToS3();
+        }
+
+        private static void UploadFileToS3()
+        {
+            AmazonS3Client s3Client = new AmazonS3Client();
+
+            s3Client.SendFileToS3();
+        }
+
+        private static void DownloadGoogleBookFile()
+        {
             GoogleDriveClient client = new GoogleDriveClient();
 
             client.DownloadBookCsv();
