72d3a74718e70a9db3978b39d740e01d6e0a30b8
Simon Ng <simon.ng@fairfaxmedia.com.au>
2016-11-03 11:58:03 +1100

    Adding in GoogleSheetsDownloader

---
+++ MyBookLibrary.GoogleSheetDownloader/Program.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MyBookLibrary.Data;
+using MyBookLibrary.RestClients;
+using MyBookLibrary.Service;
+using MyBookLibrary.Service.Mapper;
+
+namespace MyBookLibrary.GoogleSheetDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile(new MapToModelProfile()));
+
+            GoogleDriveClient client = new GoogleDriveClient();
+
+            client.DownloadBookCsv();
+
+            JsonFileGenerator fileGenerator = new JsonFileGenerator(new BookReadService(new LocalDatabaseReader()));
+
+            fileGenerator.GenerateJsonDataFile();
+            fileGenerator.PersistGoogleBooksDataIntoFile();
+        }
+    }
+}
--- MyBookLibrary.Service/JsonFileGenerator.cs
+++ MyBookLibrary.Service/JsonFileGenerator.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using MyBookLibrary.Data;
 using MyBookLibrary.Data.Dtos;
 using MyBookLibrary.RestClients;
