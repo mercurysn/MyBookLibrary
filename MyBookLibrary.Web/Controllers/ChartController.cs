c2d1d657c79b7aa8771558e41131c533d1e5978c
Simon Ng <mercury_sn@hotmail.com>
2017-02-25 23:47:30 +1100

    Added Chart Controller

---
+++ MyBookLibrary.Web/Controllers/ChartController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MyBookLibrary.Web.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
